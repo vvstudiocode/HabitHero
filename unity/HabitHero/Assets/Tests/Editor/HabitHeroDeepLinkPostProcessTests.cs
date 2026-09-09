c592fbdfada7c67fd08001a4178a5723807d45cd
guanwei <vvstudiocode@gmail.com>
2026-09-09 10:35:54 +0800

    test: require valid Unity iOS plist output

---
+++ unity/HabitHero/Assets/Tests/Editor/HabitHeroDeepLinkPostProcessTests.cs
@@ -0,0 +1,28 @@
+#if UNITY_EDITOR
+using System.Xml;
+using HabitHero.Editor;
+using NUnit.Framework;
+
+namespace HabitHero.Tests
+{
+    public sealed class HabitHeroDeepLinkPostProcessTests
+    {
+        [Test]
+        public void IosPlistNormalizationRemovesUnityEmptyInternalSubset()
+        {
+            const string malformedHeader =
+                "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+                "<!DOCTYPE plist PUBLIC \"-//Apple Computer//DTD PLIST 1.0//EN\" " +
+                "\"http://www.apple.com/DTDs/PropertyList-1.0.dtd\"[]>\n" +
+                "<plist version=\"1.0\"><dict /></plist>";
+
+            string normalized = HabitHeroDeepLinkPostProcess.NormalizeIosPlistXml(
+                malformedHeader);
+
+            StringAssert.DoesNotContain("\"[]>", normalized);
+            XmlDocument document = new XmlDocument();
+            Assert.DoesNotThrow(() => document.LoadXml(normalized));
+        }
+    }
+}
+#endif
